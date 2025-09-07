48caeb31e85ba8b374a2c8018c71e7406b99e46e
Benyberg11 <115043937+Benyberg11@users.noreply.github.com>
2025-09-07 23:31:14 +0200

    Halfway exercises
    Yes

--- week1/handout/src/APL/Eval.hs
+++ week1/handout/src/APL/Eval.hs
@@ -5,9 +5,45 @@ where
 
 import APL.AST (Exp(..))
 
+type Error = String
+
 data Val
   = ValInt Integer
   deriving (Eq, Show)
 
-eval :: Exp -> Val
-eval (CstInt x) = ValInt x
+eval :: Exp -> Either Error Val
+
+eval (CstInt x) = Right $ ValInt x
+
+eval (Add e1 e2) =
+  case (eval e1, eval e2) of
+    (Right (ValInt x), Right (ValInt y)) -> Right (ValInt (x + y))
+    (Left err, _) -> Left err
+    (_, Left err) -> Left err
+
+eval (Sub e1 e2) =
+  case (eval e1, eval e2) of
+    (Right (ValInt x), Right (ValInt y)) -> Right (ValInt (x - y))
+    (Left err, _) -> Left err
+    (_, Left err) -> Left err
+
+eval (Mul e1 e2) =
+  case (eval e1, eval e2) of
+    (Right (ValInt x), Right (ValInt y)) -> Right (ValInt (x * y))
+    (Left err, _) -> Left err
+    (_, Left err) -> Left err
+
+eval (Div e1 e2) =
+  case (eval e1, eval e2) of
+    (Right (ValInt _), Right (ValInt 0)) -> Left "Division by zero"
+    (Right (ValInt x), Right (ValInt y)) -> Right (ValInt (x `div` y))
+    (Left err, _) -> Left err
+    (_, Left err) -> Left err
+
+eval (Pow e1 e2) =
+  case (eval e1, eval e2) of
+    (Right (ValInt x), Right (ValInt y))
+      | y < 0     -> Left "Negative exponent"
+      | otherwise -> Right (ValInt (x ^ y))
+    (Left err, _) -> Left err
+    (_, Left err) -> Left err
--- week1/handout/src/APL/Eval_Tests.hs
+++ week1/handout/src/APL/Eval_Tests.hs
@@ -4,8 +4,22 @@ import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.HUnit (testCase, (@?=))
 import APL.AST (Exp(..))
 import APL.Eval (Val(..), eval)
+
 tests :: TestTree
 tests =
-  testGroup
-    "Evaluation"
-    []
+ testGroup "Evaluation"
+    [ testCase "CstInt evaluates" $
+        eval (CstInt 42) @?=Right (ValInt 42)
+
+        , testCase "Addition works" $
+        eval (Add (CstInt 5) (CstInt 7)) @?= Right (ValInt 12)
+
+        , testCase "Subtraction works" $
+        eval (Sub (CstInt 10) (CstInt 3)) @?= Right (ValInt 7)
+
+        , testCase "Division by zero fails" $
+        eval (Div (CstInt 5) (CstInt 0)) @?= Left "Division by zero"
+
+        , testCase "Negative exponent fails" $
+        eval (Pow (CstInt 2) (CstInt (-3))) @?= Left "Negative exponent"
+    ]
